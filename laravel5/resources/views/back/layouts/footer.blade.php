41b8174c1fe9431c81d3f244845ce3e112dda40f
yazdi <aminashofteh@gmail.com>
2020-10-29 14:02:08 +0330

    complete admin firstPage and related routes

---
+++ laravel5/resources/views/back/layouts/footer.blade.php
@@ -0,0 +1,22 @@
+       
+        </div>
+        <!-- main-panel ends -->
+      </div>
+      <!-- page-body-wrapper ends -->
+    </div>
+    <!-- container-scroller -->
+    <!-- plugins:js -->
+    <script src="{{ url('back/assets/vendors/js/vendor.bundle.base.js') }}"></script>
+    <script src="{{ url('back/assets/vendors/js/vendor.bundle.addons.js') }}"></script>
+    <!-- endinject -->
+    <!-- Plugin js for this page-->
+    <!-- End plugin js for this page-->
+    <!-- inject:js -->
+    <script src="{{ url('back/assets/js/shared/off-canvas.js') }}"></script>
+    <script src="{{ url('back/assets/js/shared/misc.js') }}"></script>
+    <!-- endinject -->
+    <!-- Custom js for this page-->
+    <script src="{{ url('back/assets/js/demo_1/dashboard.js') }}"></script>
+    <!-- End custom js for this page-->
+  </body>
+</html>
